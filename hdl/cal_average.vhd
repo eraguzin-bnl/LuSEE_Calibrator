d5b7876acacb9ceacb52ef5fd54ad3f27856d632
Eric <eraguzin@bnl.gov>
2024-01-10 17:37:27 -0500

    Added additional logic for cal_average block. Requires a bunch of FIFOs, but did all of the regular multipliations. Next need to add a RAM block to keep sums, and the logic for the final cycle where more multiplications are needed

--- hdl/cal_average.vhd
+++ hdl/cal_average.vhd
@@ -19,6 +19,7 @@
 library IEEE;
 
 use IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.ALL;
 
 entity cal_average is
   PORT( clk                               :   IN    std_logic;
@@ -33,6 +34,9 @@ entity cal_average is
         kar                               :   IN    std_logic_vector(15 DOWNTO 0);  -- ufix16
         readyout                          :   IN    std_logic;
         readycal                          :   IN    std_logic;
+        index1                            :   IN    std_logic_vector(5 downto 0);
+        index2                            :   IN    std_logic_vector(5 downto 0);
+        index3                            :   IN    std_logic_vector(5 downto 0);
         outreal                           :   OUT   std_logic_vector(31 DOWNTO 0);  -- sfix32_En24
         outimag                           :   OUT   std_logic_vector(31 DOWNTO 0);  -- sfix32_En24
         powertop                          :   OUT   std_logic_vector(31 DOWNTO 0);  -- ufix32_En18
@@ -77,12 +81,66 @@ architecture architecture_cal_average of cal_average is
     SIGNAL kar_readyout_data_s                      : std_logic_vector(16 DOWNTO 0);
     SIGNAL kar_readyout_data_out                    : std_logic_vector(16 DOWNTO 0);
     
+    SIGNAL kar_curr                                : unsigned(15 DOWNTO 0);
+    SIGNAL kar_squared                             : unsigned(31 DOWNTO 0);
+    SIGNAL kar_squared_signed                      : signed(31 DOWNTO 0);
+    SIGNAL readyout_curr                           : std_logic; 
+    SIGNAL tick                                    : std_logic;
+    
     SIGNAL readyin_s                            : std_logic;
     SIGNAL readycal_s                           : std_logic;
     SIGNAL error_data_fifo_full                 : std_logic;
     SIGNAL error_data_fifo_backup               : std_logic;
     SIGNAL error_phase_fifo_full                 : std_logic;
     SIGNAL error_phase_fifo_backup               : std_logic;
+    SIGNAL error_fifo_alignment                 : std_logic;
+    
+    SIGNAL product_re_re                   : std_logic_vector(63 DOWNTO 0);
+    SIGNAL product_re_im                   : std_logic_vector(63 DOWNTO 0);
+    SIGNAL product_im_re                   : std_logic_vector(63 DOWNTO 0);
+    SIGNAL product_im_im                   : std_logic_vector(63 DOWNTO 0);
+    
+    SIGNAL multiplicand1_re                 : std_logic_vector(31 DOWNTO 0);
+    SIGNAL multiplicand1_im                 : std_logic_vector(31 DOWNTO 0);
+    SIGNAL multiplicand2_re                 : std_logic_vector(31 DOWNTO 0);
+    SIGNAL multiplicand2_im                 : std_logic_vector(31 DOWNTO 0);
+    
+    SIGNAL sum_re                          : signed(64 DOWNTO 0);
+    SIGNAL sum_im                          : signed(64 DOWNTO 0);
+    SIGNAL valid_in                        : std_logic;
+    SIGNAL valid_out                       : std_logic_vector(3 DOWNTO 0);
+    
+    SIGNAL sum0_re_new                        : signed(37 DOWNTO 0);
+    SIGNAL sum0alt_re_new                     : signed(37 DOWNTO 0);
+    SIGNAL sum1_re_new                        : signed(37 DOWNTO 0);
+    SIGNAL sum2_re_new                        : signed(37 DOWNTO 0);
+    
+    SIGNAL sum0_im_new                        : signed(37 DOWNTO 0);
+    SIGNAL sum0alt_im_new                     : signed(37 DOWNTO 0);
+    SIGNAL sum1_im_new                        : signed(37 DOWNTO 0);
+    SIGNAL sum2_im_new                        : signed(37 DOWNTO 0);
+    
+    SIGNAL slice1                          : integer range 0 to 33;
+    SIGNAL slice2                          : integer range 0 to 33;
+    SIGNAL slice3                          : integer range 0 to 33;
+    SIGNAL cplx_in_re                      : signed(31 DOWNTO 0);
+    SIGNAL cplx_in_im                      : signed(31 DOWNTO 0);
+    SIGNAL cplx_in_re_rev                  : signed(31 DOWNTO 0);
+    SIGNAL cplx_in_im_rev                  : signed(31 DOWNTO 0);
+    
+    type state_type is (S_IDLE,
+        S_FIFO_WAIT_1,
+        S_FIFO_WAIT_2,
+        S_BEGIN_MULTIPLY,
+        S_MULTIPLY_WAIT,
+        S_BIT_SLICE,
+        S_BEGIN_MULTIPLY_2,
+        S_MULTIPLY_WAIT_2,
+        S_BIT_SLICE_2,
+        S_WAIT_FOR_RESULT6,
+        S_ACT_ON_RESULT1,
+        S_ACT_ON_RESULT2);
+    signal state: state_type;
 
 begin
 
@@ -143,12 +201,88 @@ begin
     PORT MAP( 
         CLK      => clk,
         RESET_N  => not reset,
-        DATA     => kar_readyout_data_im_s,
-        WE       => kar_readyout_data_im_we,
-        FULL     => kar_readyout_data_im_full,
-        Q        => kar_readyout_data_im_out,
-        RE       => kar_readyout_data_im_re,
-        EMPTY    => kar_readyout_data_im_empty
+        DATA     => kar_readyout_data_s,
+        WE       => kar_readyout_data_we,
+        FULL     => kar_readyout_data_full,
+        Q        => kar_readyout_data_out,
+        RE       => kar_readyout_data_re,
+        EMPTY    => kar_readyout_data_empty
+        );
+        
+    --Real of first * real of second
+    mult_re_re : entity work.Multiply_generic32
+        generic map(
+            size => 32)
+        port map(
+            -- Inputs
+            i_clk => clk,
+            i_rstb => reset,
+            i_ma => multiplicand1_re,
+            i_mb => multiplicand2_re,
+
+            --Valid
+            valid_in => valid_in,
+            valid_out => valid_out(0),
+
+            -- Outputs
+            o_m => product_re_re
+        );
+        
+    --Real of first * imaginary of second
+    mult_re_im : entity work.Multiply_generic32
+        generic map(
+            size => 32)
+        port map(
+            -- Inputs
+            i_clk => clk,
+            i_rstb => reset,
+            i_ma => multiplicand1_re,
+            i_mb => multiplicand2_im,
+
+            --Valid
+            valid_in => valid_in,
+            valid_out => valid_out(1),
+
+            -- Outputs
+            o_m => product_re_im
+        );
+        
+    --Imaginary of first * real of second
+    mult_im_re : entity work.Multiply_generic32
+        generic map(
+            size => 32)
+        port map(
+            -- Inputs
+            i_clk => clk,
+            i_rstb => reset,
+            i_ma => multiplicand1_im,
+            i_mb => multiplicand2_re,
+
+            --Valid
+            valid_in => valid_in,
+            valid_out => valid_out(2),
+
+            -- Outputs
+            o_m => product_im_re
+        );
+        
+    --Imaginary of first * imaginary of second
+    mult_im_im : entity work.Multiply_generic32
+        generic map(
+            size => 32)
+        port map(
+            -- Inputs
+            i_clk => clk,
+            i_rstb => reset,
+            i_ma => multiplicand1_im,
+            i_mb => multiplicand2_im,
+
+            --Valid
+            valid_in => valid_in,
+            valid_out => valid_out(3),
+
+            -- Outputs
+            o_m => product_im_im
         );
 
     process (clk) begin
@@ -166,11 +300,43 @@ begin
                 
                 kar_readyout_data_s              <= (others=>'0');
                 kar_readyout_data_we             <= '0';
+                valid_in                      <= '0';
+                
+                multiplicand1_re             <= (others=>'0');
+                multiplicand1_im             <= (others=>'0');
+                multiplicand2_re             <= (others=>'0');
+                multiplicand2_im             <= (others=>'0');
+                
+                kar_curr                     <= (others=>'0');
+                kar_squared                  <= (others=>'0');
+                kar_squared_signed           <= (others=>'0');
+                readyout_curr                <= '0';
+                
+                tick                         <= '1';
+                cplx_in_re                   <= (others=>'0');
+                cplx_in_im                   <= (others=>'0');
+                cplx_in_re_rev               <= (others=>'0');
+                cplx_in_im_rev               <= (others=>'0');
+                
+                slice1                       <= 0;
+                slice2                       <= 0;
+                slice3                       <= 0;
+                
+                sum0_re_new                  <= (others=>'0');
+                sum0alt_re_new               <= (others=>'0');
+                sum1_re_new                  <= (others=>'0');
+                sum2_re_new                  <= (others=>'0');
+                
+                sum0_im_new                  <= (others=>'0');
+                sum0alt_im_new               <= (others=>'0');
+                sum1_im_new                  <= (others=>'0');
+                sum2_im_new                  <= (others=>'0');
                 
                 error_data_fifo_full         <= '0';
                 error_data_fifo_backup       <= '0';
                 error_phase_fifo_full         <= '0';
                 error_phase_fifo_backup       <= '0';
+                error_fifo_alignment         <= '0';
                 readyin_s                    <= '0';
                 readycal_s                   <= '0';
             else
@@ -223,6 +389,101 @@ begin
                     -- When a new batch comes in, both FIFOs should be completely empty
                     error_phase_fifo_backup <= '1';
                 end if;
+                
+                case state is
+                when S_IDLE =>
+                    -- Wait until we have phase data that has been FIFO'data
+                    -- If there is phase data, there should ALWAYS be notch data
+                    notch_data_re_re <= '0';
+                    notch_data_im_re <= '0';
+                    phase_data_re_re <= '0';
+                    phase_data_im_re <= '0';
+                    kar_readyout_data_re <= '0';
+                    if (phase_data_re_empty='0') then
+                        if (phase_data_im_empty='1' or kar_readyout_data_empty='1' or notch_data_re_empty='1' or notch_data_im_empty='1') then
+                            error_fifo_alignment <= '1';
+                        end if;
+                        
+                        -- Get the next output for all FIFOs
+                        notch_data_re_re <= '1';
+                        notch_data_im_re <= '1';
+                        phase_data_re_re <= '1';
+                        phase_data_im_re <= '1';
+                        kar_readyout_data_re <= '1';
+                        
+                        state <= S_FIFO_WAIT_1;
+                    end if;
+                when S_FIFO_WAIT_1 =>
+                    notch_data_re_re <= '0';
+                    notch_data_im_re <= '0';
+                    phase_data_re_re <= '0';
+                    phase_data_im_re <= '0';
+                    kar_readyout_data_re <= '0';
+                    
+                    state <= S_FIFO_WAIT_2;
+                when S_FIFO_WAIT_2 =>
+                    state <= S_BEGIN_MULTIPLY;
+                when S_BEGIN_MULTIPLY =>
+                    multiplicand1_re <= notch_data_re_out;
+                    multiplicand1_im <= notch_data_im_out;
+                    multiplicand2_re <= phase_data_re_out;
+                    multiplicand2_im <= phase_data_im_out;
+                    valid_in <= '1';
+                    
+                    kar_curr <= unsigned(kar_readyout_data_out(15 DOWNTO 0));
+                    readyout_curr <= kar_readyout_data_out(16);
+                    state <= S_MULTIPLY_WAIT;
+                when S_MULTIPLY_WAIT =>
+                    valid_in <= '0';
+                    if (valid_out = "1111") then
+                        sum_re <= resize(signed(product_re_re), 65) - resize(signed(product_im_im), 65);
+                        sum_im <= resize(signed(product_re_im), 65) + resize(signed(product_im_re), 65);
+                        kar_squared <= kar_curr * kar_curr;
+                        state <= S_BIT_SLICE;
+                    end if;
+                    slice1 <= to_integer(unsigned(index1));
+                when S_BIT_SLICE =>
+                    cplx_in_re <= sum_re(64 DOWNTO 33);
+                    cplx_in_im <= sum_im(64 DOWNTO 33);
+                    kar_squared_signed <= signed(kar_squared);
+                    state <= S_BEGIN_MULTIPLY_2;
+                when S_BEGIN_MULTIPLY_2 =>
+                    multiplicand1_re <= std_logic_vector(cplx_in_re);
+                    multiplicand1_im <= std_logic_vector(cplx_in_im);
+                    multiplicand2_re <= std_logic_vector(-kar_squared_signed);
+                    multiplicand2_im <= x"0000" & std_logic_vector(kar_curr);
+                    valid_in <= '1';
+                    
+                    state <= S_MULTIPLY_WAIT_2;
+                when S_MULTIPLY_WAIT_2 =>
+                    valid_in <= '0';
+                    if (valid_out = "1111") then
+                        state <= S_BIT_SLICE_2;
+                    end if;
+                    slice2 <= to_integer(unsigned(index2));
+                when S_BIT_SLICE_2 =>
+                    sum0_re_new <= cplx_in_re;
+                    sum0_im_new <= cplx_in_im;
+                    if (tick) then
+                        sum0alt_re_new <= cplx_in_re;
+                        sum0alt_im_new <= cplx_in_im;
+                    else
+                        sum0alt_re_new <= -cplx_in_re;
+                        sum0alt_im_new <= -cplx_in_im;
+                    end if;
+                    tick <= not tick;
+                    
+                    --Todo, do the negative part in the next step when adding
+                    sum1_re_new <= -1 * signed(product_im_im(63 DOWNTO 32));
+                    sum1_im_new <= signed(product_re_im(63 DOWNTO 32));
+                    
+                    sum2_re_new <= signed(product_re_re(63 DOWNTO 32));
+                    sum2_im_new <= signed(product_im_re(63 DOWNTO 32));
+                    
+                    state <= S_BEGIN_MULTIPLY_2;
+                when others =>		
+                    state <= S_IDLE;
+                end case;
             end if;
         end if;
     end process;
