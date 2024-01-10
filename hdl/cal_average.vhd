9e3d161eb6d1a7517d43c8ef4e52a7cfcc38651f
Eric <eraguzin@bnl.gov>
2024-01-10 14:45:52 -0500

    Added FIFOs for incoming phase data and coordinating all that

--- hdl/cal_average.vhd
+++ hdl/cal_average.vhd
@@ -56,9 +56,33 @@ architecture architecture_cal_average of cal_average is
     SIGNAL notch_data_im_s                      : std_logic_vector(31 DOWNTO 0);
     SIGNAL notch_data_im_out                    : std_logic_vector(31 DOWNTO 0);
     
+    SIGNAL phase_data_re_we                     : std_logic;
+    SIGNAL phase_data_re_re                     : std_logic;
+    SIGNAL phase_data_re_full                   : std_logic;
+    SIGNAL phase_data_re_empty                  : std_logic;
+    SIGNAL phase_data_re_s                      : std_logic_vector(31 DOWNTO 0);
+    SIGNAL phase_data_re_out                    : std_logic_vector(31 DOWNTO 0);
+    
+    SIGNAL phase_data_im_we                     : std_logic;
+    SIGNAL phase_data_im_re                     : std_logic;
+    SIGNAL phase_data_im_full                   : std_logic;
+    SIGNAL phase_data_im_empty                  : std_logic;
+    SIGNAL phase_data_im_s                      : std_logic_vector(31 DOWNTO 0);
+    SIGNAL phase_data_im_out                    : std_logic_vector(31 DOWNTO 0);
+    
+    SIGNAL kar_readyout_data_we                     : std_logic;
+    SIGNAL kar_readyout_data_re                     : std_logic;
+    SIGNAL kar_readyout_data_full                   : std_logic;
+    SIGNAL kar_readyout_data_empty                  : std_logic;
+    SIGNAL kar_readyout_data_s                      : std_logic_vector(16 DOWNTO 0);
+    SIGNAL kar_readyout_data_out                    : std_logic_vector(16 DOWNTO 0);
+    
     SIGNAL readyin_s                            : std_logic;
+    SIGNAL readycal_s                           : std_logic;
     SIGNAL error_data_fifo_full                 : std_logic;
     SIGNAL error_data_fifo_backup               : std_logic;
+    SIGNAL error_phase_fifo_full                 : std_logic;
+    SIGNAL error_phase_fifo_backup               : std_logic;
 
 begin
 
@@ -66,7 +90,7 @@ begin
     -- Stored in a FIFO and the state machine does the math as it can
     -- This FIFO has room for 512 samples of values. 
     -- It should never fill up, because the cal_phaser block should be sending the phase data soon after a batch is coming in
-    notch_data_re : entity work.CAL_AVERAGE_NOTCH_FIFO
+    notch_data_re : entity work.CAL_AVERAGE_DATA_FIFO
     PORT MAP( 
         CLK      => clk,
         RESET_N  => not reset,
@@ -78,7 +102,7 @@ begin
         EMPTY    => notch_data_re_empty
         );
         
-    notch_data_im : entity work.CAL_AVERAGE_NOTCH_FIFO
+    notch_data_im : entity work.CAL_AVERAGE_DATA_FIFO
     PORT MAP( 
         CLK      => clk,
         RESET_N  => not reset,
@@ -89,6 +113,43 @@ begin
         RE       => notch_data_im_re,
         EMPTY    => notch_data_im_empty
         );
+        
+    phase_data_re : entity work.CAL_AVERAGE_DATA_FIFO
+    PORT MAP( 
+        CLK      => clk,
+        RESET_N  => not reset,
+        DATA     => phase_data_re_s,
+        WE       => phase_data_re_we,
+        FULL     => phase_data_re_full,
+        Q        => phase_data_re_out,
+        RE       => phase_data_re_re,
+        EMPTY    => phase_data_re_empty
+        );
+        
+    phase_data_im : entity work.CAL_AVERAGE_DATA_FIFO
+    PORT MAP( 
+        CLK      => clk,
+        RESET_N  => not reset,
+        DATA     => phase_data_im_s,
+        WE       => phase_data_im_we,
+        FULL     => phase_data_im_full,
+        Q        => phase_data_im_out,
+        RE       => phase_data_im_re,
+        EMPTY    => phase_data_im_empty
+        );
+        
+    --17 bits, 16 for kar, 1 for readyout
+    kar_readyout : entity work.CAL_AVERAGE_OTHER_FIFO
+    PORT MAP( 
+        CLK      => clk,
+        RESET_N  => not reset,
+        DATA     => kar_readyout_data_im_s,
+        WE       => kar_readyout_data_im_we,
+        FULL     => kar_readyout_data_im_full,
+        Q        => kar_readyout_data_im_out,
+        RE       => kar_readyout_data_im_re,
+        EMPTY    => kar_readyout_data_im_empty
+        );
 
     process (clk) begin
         if (rising_edge(clk)) then
@@ -98,9 +159,20 @@ begin
                 notch_data_im_s              <= (others=>'0');
                 notch_data_im_we             <= '0';
                 
+                phase_data_re_s              <= (others=>'0');
+                phase_data_re_we             <= '0';
+                phase_data_im_s              <= (others=>'0');
+                phase_data_im_we             <= '0';
+                
+                kar_readyout_data_s              <= (others=>'0');
+                kar_readyout_data_we             <= '0';
+                
                 error_data_fifo_full         <= '0';
                 error_data_fifo_backup       <= '0';
+                error_phase_fifo_full         <= '0';
+                error_phase_fifo_backup       <= '0';
                 readyin_s                    <= '0';
+                readycal_s                   <= '0';
             else
                 -- This section will just put any incoming bin of 2, 6, 10, 14, into the FIFO for processing
                 -- And throw an error if it ever fills up
@@ -125,6 +197,31 @@ begin
                 if (readyin_s = '0' and readyin = '1' and (notch_data_re_empty = '0' or notch_data_im_empty = '0')) then
                     -- When a new batch comes in, both FIFOs should be completely empty
                     error_data_fifo_backup <= '1';
+                end if;
+                
+                phase_data_re_we <= '0';
+                phase_data_im_we <= '0';
+                kar_readyout_data_we <= '0';
+                if (readycal = '1') then
+                    if (bin_in(1 downto 0) = "10") then
+                        -- Save incoming data so we can multiply when we have the phase later
+                        if (phase_data_re_full = '0' and phase_data_im_full = '0' and kar_readyout_data_full = '0') then
+                            phase_data_re_s <= phase_cor_re;
+                            phase_data_re_we <= '1';
+                            phase_data_im_s <= phase_cor_im;
+                            phase_data_im_we <= '1';
+                            kar_readyout_data_s <= readyout & kar;
+                            kar_readyout_data_we <= '1';
+                        else
+                            --With a depth of 512 and enough time between notch filter averages to process, should never fill up
+                            error_phase_fifo_full <= '1';
+                        end if;
+                    end if;
+                end if;
+                readycal_s <= readycal;
+                if (readycal_s='0' and readycal='1' and (phase_data_re_empty='0' or phase_data_im_empty='0' or kar_readyout_data_empty='0')) then
+                    -- When a new batch comes in, both FIFOs should be completely empty
+                    error_phase_fifo_backup <= '1';
                 end if;
             end if;
         end if;
